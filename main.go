be633666394767ba89c4568a213d57bd2aa14ddc
Angela Hao <angelahao@google.com>
2023-08-16 15:44:57 -0700

    Removed password parameter, added mac script and base64

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"log"
 
@@ -14,11 +15,10 @@ func main() {
 	}
 	mode := flag.String("mode", "", "the flag for action: -e for encryption or -d for decryption")
 	msg := flag.String("msg", "", "the message to encrypt or decrypt")
-	password := flag.String("password", "", "the password to use encryption or decryption")
 	flag.Parse()
 
-	if *mode == "" || *msg == "" || *password == "" {
-		log.Printf("Usage: go run main.go -mode <-e|-d> -msg <msg> -password <password>")
+	if *mode == "" || *msg == "" {
+		log.Printf("Usage: go run main.go -mode <-e|-d> -msg <msg>")
 		return
 	}
 
@@ -34,14 +34,19 @@ func main() {
 			log.Printf("Encrypt: got %v, want nil err", err)
 			return
 		}
-		log.Printf("Encrypted message: %v", ciphertext)
+		str := base64.StdEncoding.EncodeToString(ciphertext)
+		log.Printf("Encrypted message: %v", str)
 	} else {
-		byteSlice := []byte(*msg)
-		plaintext, err := key.Decrypt(byteSlice)
+		str, err := base64.StdEncoding.DecodeString(*msg)
+		if err != nil {
+			log.Printf("Base64: got %v, want nil err", err)
+		}
+		plaintext, err := key.Decrypt([]byte(str))
 		if err != nil {
 			log.Printf("Decrypt: got %v, want nil err", err)
 			return
 		}
-		log.Printf("Decrypted message: %v", plaintext)
+		plainString := string(plaintext)
+		log.Printf("Decrypted message: %v", plainString)
 	}
 }
