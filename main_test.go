cdf6ae390568de4ec53cda2dfde8c177d4742940
Liam Forges <lforges@google.com>
2023-08-21 20:05:29 +0000

    added decrypt unit test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/googleapis/enterprise-certificate-proxy/client"
@@ -17,10 +18,34 @@ func TestEncrypt(t *testing.T) {
 	}
 	msg := "This is my secret message"
 	byteSlice := []byte(msg)
+	_, err = key.Encrypt(byteSlice)
+	if err != nil {
+		t.Errorf("Encrypt: got %v, want nil err", err)
+		return
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	key, err := client.Cred(linuxConfigFilePath)
+	if err != nil {
+		t.Errorf("Cred: got %v, want nil err", err)
+		return
+	}
+	msg := "This is my secret message"
+	byteSlice := []byte(msg)
 	ciphertext, err := key.Encrypt(byteSlice)
 	if err != nil {
 		t.Errorf("Encrypt: got %v, want nil err", err)
 		return
 	}
-	t.Logf("Encrypted: %v", ciphertext)
+	decrypted, err := key.Decrypt(ciphertext)
+	if err != nil {
+		t.Errorf("Decrypt: got %v want nil err", err)
+		return
+	}
+	decrypted = bytes.Trim(decrypted, "\x00")
+	if string(decrypted) != msg {
+		t.Errorf("Decrypt error expected: %v, got: %v", msg, string(decrypted))
+	}
+
 }
